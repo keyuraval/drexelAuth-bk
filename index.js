81b44010aa35dcae1a43d7fd465d4c59ebd6626e
Keyur Raval <ravalkeyur054@gmail.com>
2023-04-23 22:38:28 +0530

    ICT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const sendMail = require("./controllers/sendMails");
 
 // database connection
 connection();
@@ -16,6 +17,7 @@ app.use(cors());
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.get("/mail", sendMail)
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
